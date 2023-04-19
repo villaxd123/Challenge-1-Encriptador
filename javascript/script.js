f4ec830bc9d893907030b88f53c5462bf0e140cc
Jorge Villafuerte <jorge_123_386@hotmail.es>
2023-04-19 00:15:05 -0400

    Cambios referentes al anterior commit: index.html - completado y reestructurado. style.css - completado y resposivo. script.js - finalizado cumpliendo los requerimientos. pictures (carpeta) - imagen de alura reemplazada por la de linkedin.

---
+++ javascript/script.js
@@ -0,0 +1,138 @@
+// Función que encripta el texto.
+function encriptarTexto(texto) {
+    let nuevoString = "";
+
+    for (let i = 0; i < texto.length; i++) {
+        switch (texto[i]) {
+            case "a":
+                nuevoString = nuevoString + "ai";
+                break;
+            case "e":
+                nuevoString = nuevoString + "enter";
+                break;
+            case "i":
+                nuevoString = nuevoString + "imes";
+                break;
+            case "o":
+                nuevoString = nuevoString + "ober";
+                break;
+            case "u":
+                nuevoString = nuevoString + "ufat";
+                break;
+            default:
+                nuevoString = nuevoString + texto[i];
+                break;
+        }
+    }
+
+    return nuevoString;
+}
+// Función que desencripta el texto.
+function desencriptarTexto(texto) {
+    let nuevoString = "";
+
+    for (let i = 0; i < texto.length; i++) {
+        switch (texto[i]) {
+            case "a":
+                if (texto[i + 1] == "i") {
+                    nuevoString = nuevoString + "a";
+                    i++;
+                }
+                break;
+            case "e":
+                if (texto[i + 1] == "n" && texto[i + 2] == "t" && texto[i + 3] == "e" && texto[i + 4] == "r") {
+                    nuevoString = nuevoString + "e";
+                    i = i + 4;
+                }
+                break;
+            case "i":
+                if (texto[i + 1] == "m" && texto[i + 2] == "e" && texto[i + 3] == "s") {
+                    nuevoString = nuevoString + "i";
+                    i = i + 3;
+                }
+                break;
+            case "o":
+                if (texto[i + 1] == "b" && texto[i + 2] == "e" && texto[i + 3] == "r") {
+                    nuevoString = nuevoString + "o";
+                    i = i + 3;
+                }
+                break;
+            case "u":
+                if (texto[i + 1] == "f" && texto[i + 2] == "a" && texto[i + 3] == "t") {
+                    nuevoString = nuevoString + "u";
+                    i = i + 3;
+                }
+                break;
+            default:
+                nuevoString = nuevoString + texto[i];
+                break;
+        }
+    }
+
+    return nuevoString;
+}
+// Función llamada al precionar el botón de "Encriptar".
+function textoEntrada() {
+    let entrada = document.getElementById("entrada");
+    let salida = document.getElementById("salida");
+    let texto = entrada.value;
+
+    if (texto.trim() == "") {
+        alert("No ingresó ningún texto para encriptar.");
+    } else {
+        ocultarImagen();
+    }
+
+    let resultadoTexto = encriptarTexto(texto);
+    salida.value = resultadoTexto;
+}
+// Función llamada al precionar el botón de "Desencriptar".
+function textoSalida() {
+    let salida = document.getElementById("salida");
+    let texto = entrada.value;
+
+    if (texto.trim() == "") {
+        alert("No ingresó ningún texto para desencriptar.");
+    }
+
+    /* Copiar del portapapeles (No es totalmente necesario).
+    navigator.clipboard
+        .readText()
+        .then((text) => {
+            document.getElementById("entrada").value = text;
+        })
+        .catch((error) => {
+            alert("Error al leer del portapapeles:", error);
+        });
+    */
+
+    let resultadoTexto = desencriptarTexto(texto);
+    salida.value = resultadoTexto;
+}
+// Función llamada al precionar el botón de "Copiar".
+function copiarTexto() {
+    let salida = document.getElementById("salida");
+    let texto = salida.value;
+
+    // Copiar texto al portapapeles.
+    navigator.clipboard
+        .writeText(texto)
+        .then(() => {
+            if (texto.trim() == "") {
+                alert("No se encontró algún texto para copiar.");
+            }
+        })
+        .catch((error) => {
+            alert("Error al copiar el texto:", error);
+        });
+}
+// Funcion para ocultar la imagen de espera.
+function ocultarImagen() {
+    let salidaActiva = document.getElementById("salida");
+    let imagenPasiva = document.getElementById("img-1");
+    let listaImagen = document.getElementById("unactivated");
+
+    salidaActiva.style.display = "inline-block";
+    imagenPasiva.style.display = "none";
+    listaImagen.style.display = "none";
+}
